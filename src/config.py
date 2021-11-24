647c81b4d222378fe8c8f57907a4fe248dc9f1cb
Olle Welin <olle_welin@hotmail.com>
2021-11-24 18:54:14 +0100

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,30 @@
+import torch
+
+BATCH_SIZE = 2 # increase / decrease according to GPU memeory
+RESIZE_TO = 512 # resize the image for training and transforms
+NUM_EPOCHS = 50 # number of epochs to train for
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# training images and XML files directory
+
+TRAIN_DIR = '../crop/train'
+# validation images and XML files directory
+VALID_DIR = '../crop/test'
+
+# classes: 0 index is reserved for background
+
+# classes: 0 index is reserved for background
+CLASSES = [
+    'background', 'class-1', 'class-2'
+]
+NUM_CLASSES = 3
+
+
+# whether to visualize images after crearing the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = False
+
+# location to save model and plots
+OUT_DIR = '../outputs'
+SAVE_PLOTS_EPOCH = 2 # save loss plots after these many epochs
+SAVE_MODEL_EPOCH = 2 # save model after these many epochs
