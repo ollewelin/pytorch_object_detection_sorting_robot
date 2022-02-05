c5195411dd5d32b056da7e9eca8715da500e2ceb
Olle Welin <olle_welin@hotmail.com>
2022-02-05 22:08:08 +0100

    Add files via upload

---
+++ src/export_onnx.py
@@ -0,0 +1,31 @@
+import torch
+import torch.onnx #Export to ONNX
+from config import MODEL_DICT_FILE
+from model import create_model
+# set the computation device
+#device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+device = torch.device('cpu')
+# load the model and the trained weights
+model = create_model(num_classes=5).to(device)
+model.load_state_dict(torch.load(
+     MODEL_DICT_FILE, map_location=device
+))
+model.eval()
+
+dummpy_input = torch.randn(1, 3, 1333, 800)
+dummpy_input = dummpy_input.to(device)
+#model.eval()
+model(dummpy_input)
+
+# Export the model
+torch.onnx.export(model,               # model being run
+                  dummpy_input,                         # model input (or a tuple for multiple inputs)
+                  "model.onnx",   # where to save the model (can be a file or file-like object)
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=11,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['input'],   # the model's input names
+                  output_names = ['output'],) # the model's output names)
+print("=======================")
+print("Exported model to ONNX ")
+print("=======================")
